807b6589fb457472f43b29817706d7d3367d1fae
shiorin <shiorin@192.168.100.101>
2015-02-02 12:34:53 +0900

    ロガーの実装。

--- src/main/java/main/Base.java
+++ src/main/java/main/Base.java
@@ -2,16 +2,19 @@ package main;
 
 import java.util.List;
 
-import common.TweetUtils;
+import org.apache.log4j.Logger;
 
 import twitter4j.TwitterException;
 
+import common.TweetUtils;
 
 /**
  * @author shiorin
  *
  */
 public class Base {
+	private static final Logger logger = Logger.getLogger(Base.class);
+
 	public static void main(String[] args) {
 		// TODO 最新のツイートのIDを取得し、それ以降のツイートを取得
 		try {
@@ -21,11 +24,11 @@ public class Base {
 			// Botの発言を収集
 			List<String> tweetList = TweetUtils.searchTweet("#おしりんbot");
 			for (String tweet : tweetList) {
-				// utils.tweet(tweet);
+				// TweetUtils.tweet(tweet);
 				System.out.println(tweet);
 			}
 		} catch (TwitterException e) {
-			System.err.println("ツイート失敗：" + e.getMessage());
+			logger.debug("ツイート失敗：" + e.getMessage());
 		}
 	}
 }
